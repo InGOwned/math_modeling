cc2fd583fa13d50e7c9d067462cd4c8bc3fdaf3b
InGOwned <103064059+InGOwned@users.noreply.github.com>
2022-12-14 16:41:05 +0000

    14.12.2022

--- lab_7_task_4.py
+++ lab_7_task_4.py
@@ -1,14 +1,37 @@
-import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
  
 fig, ax = plt.subplots()
-frakt, = plt.plot([], [], '-', color='r')
+frakt, = plt.plot([], [], 'o', color='r')
 
 plt.axis('equal')
 
-def mn(x0, y0, C, D):
-     n = np.arange(x0, y0, 0.01)
-     x = x0**2 - y0**2 + C
-     y = 2 * x0 * y0 + D
-     
+x = []
+y = []
+
+x0 = 0.1
+y0 = 0.1
+C = 0.3
+D = 0.33
+# def mn(x0, y0, C, D):
+for i in range(100):
+    xn = x0**2 - y0**2 + C
+    yn = 2 * x0 * y0 + D
+    x.append(xn)
+    y.append(yn)
+    x0 = xn
+    y0 = yn
+    # return x, y
+
+
+ax.set_xlim(-0.4, 0.5)
+ax.set_ylim(0.2, 0.8)
+
+
+def animate(i):
+    frakt.set_data(x[:i], y[:i])
+
+ani = FuncAnimation(fig, animate, frames=100, interval=100)
+
+ani.save("Fraktal.gif")
