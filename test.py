8ec95b074e121163b2edd393a06f6ee30aba72bc
InGOwned <103064059+InGOwned@users.noreply.github.com>
2022-12-23 18:14:32 +0000

    fixed dot on ellipse

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 
 fig, ax = plt.subplots()
-figure, = plt.plot([], [], '-', color='r')
+ball, = plt.plot([], [], 'o', color='r')
 plt.axis('equal')
 
-frames = np.arange(0, np.pi * 8, 0.1)
+frames = np.arange(0, np.pi * 8, 0.06)
 
-edge = 20
+edge = 10
 ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
 
@@ -22,9 +22,9 @@ def ellipse(p, e, fi):
 
 
 def animate(i):
-    figure.set_data(ellipse(p = 3, e = 0.65, fi = i))
-    return figure,
+    ball.set_data(ellipse(p = 3, e = 0.65, fi = i))
+    return ball,
 
 
-ani = FuncAnimation(fig, animate, frames=frames, interval=300)
+ani = FuncAnimation(fig, animate, frames=frames, interval=30)
 ani.save("ellipse.gif")
