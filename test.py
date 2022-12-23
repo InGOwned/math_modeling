0df6754257f30a3228380ab9d072ad2336c3200f
InGOwned <103064059+InGOwned@users.noreply.github.com>
2022-12-23 18:10:05 +0000

    .

--- test.py
+++ test.py
@@ -6,25 +6,25 @@ fig, ax = plt.subplots()
 figure, = plt.plot([], [], '-', color='r')
 plt.axis('equal')
 
-fi = np.arange(0, np.pi * 8, 0.1)
+frames = np.arange(0, np.pi * 8, 0.1)
+
+edge = 20
+ax.set_xlim(-edge, edge)
+ax.set_ylim(-edge, edge)
 
 
 def ellipse(p, e, fi):
+    # fi = np.arange(0, np.pi * 8, 0.1)
     r = p / (1 + (e * np.cos(fi)))
-
     x = r * np.cos(fi)
     y = r * np.sin(fi)
     return x, y
 
 
 def animate(i):
-    figure.set_data(ellipse(p = 3, e = 0.65, fi = fi))
+    figure.set_data(ellipse(p = 3, e = 0.65, fi = i))
     return figure,
 
-edge = 10
-ax.set_xlim(-edge, edge)
-ax.set_ylim(-edge, edge)
 
-
-ani = FuncAnimation(fig, animate, frames=500, interval=30)
+ani = FuncAnimation(fig, animate, frames=frames, interval=300)
 ani.save("ellipse.gif")
