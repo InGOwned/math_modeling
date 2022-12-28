50cd67cc8e711ed92fc3a301da31dfaad48ebf0b
InGOwned <103064059+InGOwned@users.noreply.github.com>
2022-12-28 14:48:17 +0000

    SATURN

--- Saturn.py
+++ Saturn.py
@@ -3,28 +3,37 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 
 fig, ax = plt.subplots()
-circle1, = plt.plot([], [], '.', color='dimgray')
-circle2, = plt.plot([], [], '.', color='dimgray')
-circle3, = plt.plot([], [], '.', color='slategrey')
+circle1, = plt.plot([], [], '.', color='darkgray')
+circle2, = plt.plot([], [], '.', color='darkgray')
+circle3, = plt.plot([], [], '.', color='dimgray')
 circle4, = plt.plot([], [], '.', color='slategrey')
 circle5, = plt.plot([], [], '.', color='slategrey')
 circle6, = plt.plot([], [], '.', color='azure')
 circle7, = plt.plot([], [], '.', color='azure')
 circle8, = plt.plot([], [], '.', color='azure')
 circle9, = plt.plot([], [], '.', color='azure')
+circle10, = plt.plot([], [], '.', color='azure')
+circle11, = plt.plot([], [], '.', color='lightsteelblue')
+circle12, = plt.plot([], [], '.', color='lightsteelblue')
+Pan, = plt.plot([], [], 'o', color='lightcoral', ms=7.5)
+Daphnis, = plt.plot([], [], 'o', color='maroon', ms=7)
+Atlas, = plt.plot([], [], 'o', color='chocolate', ms=7)
+Pandora, = plt.plot([], [], 'o', color='mistyrose', ms=8)
 
-plt.plot([0], 'o', color='y', ms=50)
 
-frames = np.arange(0, np.pi * 8, 0.08)
+plt.plot([0], 'o', color='y', ms=60)
 
 plt.axis('equal')
-plt.grid()
 
-edge = 3
+edge = 2.75
 ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
 ax.set_facecolor("black")
 
+plt.title('Saturn and its rings')
+ax.text(0, 0, 'Saturn', ha='center', va='center', color='w', fontsize=15)
+
+ax.legend([Pan, Daphnis, Pandora, Atlas], ['Pan', 'Daphnis', 'Pandora', 'Atlas'])
 
 def ring(r):
     fi = np.arange(0, np.pi * 2, 0.085)
@@ -33,24 +42,47 @@ def ring(r):
     return x, y
 
 
-def sputnik(fi):
-    x = r * np.cos(fi)
-    y = r * np.sin(fi)
+def sputnik(r, fi, angle):
+    x = r * np.cos(fi + angle)
+    y = r * np.sin(fi + angle)
     return x, y
 
 
-def animate(i):
-    circles_data = [(ring(r=0.7), circle1), (ring(r=0.77), circle2), (ring(r=1), circle3), (ring(r=1.07), circle4),
-                    (ring(r=1.14), circle5), (ring(r=1.34), circle6), (ring(r=1.41), circle7), (ring(r=1.48), circle8), (ring(r=1.55), circle9)]
+speeds = {circle1: 8, circle2: 7, circle3: 6, circle4: 5, circle5: 5, circle6: 4, circle7: 3,
+          circle8: 3, circle9: 3, circle10: 3, circle11: 2, circle12: 1}
 
-    for (x, y), circle in circles_data:
-        X_rotate = x * np.cos(i) - y * np.sin(i)
-        Y_rotate = y * np.cos(i) + x * np.sin(i)
-        circle.set_data(X_rotate, Y_rotate)
+
+def animate(i):
+    circles_list = [(ring(r=0.7), circle1), (ring(r=0.77), circle2), (ring(r=0.87), circle3), (ring(r=0.97), circle4),
+                    (ring(r=1.04), circle5), (ring(r=1.14), circle6), (ring(r=1.24), circle7), (ring(r=1.32), circle8),
+                    (ring(r=1.40), circle9), (ring(r=1.48), circle10), (ring(r=1.68), circle11),
+                    (ring(r=1.76), circle12),
+                    (sputnik(r=1.68, fi=i*0.03, angle=-1), Pan),
+                    (sputnik(r=1.76, fi=i*0.03, angle=2.3), Daphnis),
+                    (sputnik(r=1.76, fi=i*0.03, angle=1.2), Atlas),
+                    (sputnik(r=2.1, fi=i*0.03, angle=3), Pandora)]
+
+    for (x, y), circle in circles_list:
+        if circle == Pan:
+            X_rotate = x * np.cos(i * 0.0921) - y * np.sin(i * 0.0921)
+            Y_rotate = y * np.cos(i * 0.0921) + x * np.sin(i * 0.0921)
+            circle.set_data(X_rotate, Y_rotate)
+        elif circle == Daphnis or circle == Atlas:
+            X_rotate = x * np.cos(i * 0.0307) - y * np.sin(i * 0.0307)
+            Y_rotate = y * np.cos(i * 0.0307) + x * np.sin(i * 0.0307)
+            circle.set_data(X_rotate, Y_rotate)
+        elif circle == Pandora:
+            X_rotate = x * np.cos(i * 0.01) - y * np.sin(i * 0.01)
+            Y_rotate = y * np.cos(i * 0.01) + x * np.sin(i * 0.01)
+            circle.set_data(X_rotate, Y_rotate)
+        else:
+            X_rotate = x * np.cos(-i * speeds.get(circle)) - y * np.sin(-i * speeds.get(circle))
+            Y_rotate = y * np.cos(-i * speeds.get(circle)) + x * np.sin(-i * speeds.get(circle))
+            circle.set_data(X_rotate, Y_rotate)
 
 
 ani = FuncAnimation(fig,
                     animate,
-                    frames=frames,
-                    interval=30)
-ani.save('Saturn_test.gif')
+                    frames=np.arange(0, np.pi * 8, 0.08),
+                    interval=60)
+ani.save('Saturn.gif')
